fdbb985df66da69c5f53a86a5bac1c7061466126
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-24 17:07:54 -0700

    created skills page

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,3 +1,31 @@
 import React from "react";
 
-export default function Skills () {}
+export default function Skills () {
+    return (
+        <section>
+            <h1>My Skills:</h1>
+            <div>
+                <h3>Front End:</h3>
+                <ul>
+                    <li>CSS</li>
+                    <li>HTML</li>
+                    <li>JavaScript</li>
+                    <li>Server-Side API's</li>
+                    <li>Third Party API's</li>
+                </ul>
+            </div>
+            <div>
+                <h3>Back End:</h3>
+                <ul>
+                    <li>Node.js</li>
+                    <li>Express.js</li>
+                    <li>MySQL</li>
+                    <li>NoSQL</li>
+                    <li>MongoDB</li>
+                    <li>PWA</li>
+                    <li>React (still learning!)</li>
+                </ul>
+            </div>
+        </section>
+    )
+}
