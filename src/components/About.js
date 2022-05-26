64495f041d77f83443b6ab6aab614c46598fe3b6
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-26 00:01:00 -0700

    styled about me page

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import React from "react";
 export default function About () {
     return (
         <section>
-        <h1>A Little Bit About Myself</h1>
-        <div>
+        <h1 className="m-3 ps-3">A Little Bit About Myself:</h1>
+        <div className="d-flex justify-content-around m-4 p-4">
             My name is Mark Protsyuk, and I am from the Sacramento Area! As you can tell, I am a software developer and I am relatively new to the game. Besides programing, some other things that I enjoy doing are drawing, creating art, cooking, and climbing. Climbing has got to be one of my favorite pass time hobbies! I enjoy spending as obsessive amount of time trying to figure out a route or a new bouldering problem. Yes, the other hobbies do indeed get my creative side working, but climbing really makes me think hard and push myself to do more than I thought I could. Hopefully this was a good little meaningful introduction about myself and I hope to introduce myself in a workspace sometime soon!
         </div>
         </section>
