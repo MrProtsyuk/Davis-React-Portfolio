fe8ddbb60d783242dbb262bcead413bf519aeb24
Your Name <markprotsyuk@Marks-MacBook-Pro-2.local>
2022-05-26 09:54:21 -0700

    styled portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -5,15 +5,15 @@ export default function Portfolio () {
     return (
         <section>
             <div>
-                <h1>Here are some Projects that I have made!</h1>
+                <h1 className="d-flex justify-content-center p-4">Here are some Projects that I have made!</h1>
             </div>
-            <div>
+            <div className="d-flex justify-content-center p-4">
                 {projects.map((project) => (
                     <a href={project.link} key={project.image}>
                         <div>
                             <img alt="Projects" src={project.image} />
                             <div>
-                                <h1>{project.title}</h1>
+                                <h1 className="h2">{project.title}</h1>
                                 <p>{project.desctiption}</p>
                             </div>
                         </div>
